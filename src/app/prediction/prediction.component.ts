e037f75194a728ed4262ba84b8b393d5404518c9
Quentin Lefevre <quentin@glooh.media>
2021-03-16 12:20:51 +0100

    create getter to access relevant data from form response

--- src/app/prediction/prediction.component.ts
+++ src/app/prediction/prediction.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-prediction',
@@ -9,9 +9,26 @@ export class PredictionComponent implements OnInit {
   constructor() {}
 
   @Input() response: any;
+  predictions: Array<any>;
 
-  constructor() { }
+  getProba(rationale: string, outcome: string) {
+    // Filter predictions array to get only specific rationale and outcome in a array then access probabilityDecimal by dot notation, multiple by 100 to get percentage and round the result
+    return Math.round(
+      this.predictions.filter(
+        (el) =>
+          el.rationale === rationale && el.outcome.coding[0].code === outcome
+      )[0].probabilityDecimal * 100
+    );
+  }
 
-  ngOnInit() {}
+  getSummary() {
+    let r = this.predictions.filter((el) => el.rationale === 'summary');
+    let outcome: string = r[0].outcome.coding[0].code;
+    let proba: number = Math.round(r[0].probabilityDecimal * 100);
+    return [outcome, proba];
+  }
 
+  ngOnInit() {
+    this.predictions = this.response.data[0].prediction;
+  }
 }
